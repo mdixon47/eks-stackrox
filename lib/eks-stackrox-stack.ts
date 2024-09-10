7d62cc908f59cf485d80e2414328242a38a9862e
Malik Dixon <mdixon47@users.noreply.github.com>
2024-09-10 18:48:25 -0400

    First commit

--- lib/eks-stackrox-stack.ts
+++ lib/eks-stackrox-stack.ts
@@ -1,16 +1,63 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib"
+import * as eks from "aws-cdk-lib/aws-eks"
+import * as ec2 from "aws-cdk-lib/aws-ec2"
+import * as iam from "aws-cdk-lib/aws-iam"
 
 export class EksStackroxStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    // Create a new VPC for the EKS Cluster
+    const vpc = new ec2.Vpc(this, "EksVpc", {
+      maxAzs: 3, // Default is all AZs in the region
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EksStackroxQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // IAM role for EKS Cluster Admin
+    const clusterAdmin = new iam.Role(this, "ClusterAdminRole", {
+      assumedBy: new iam.AccountRootPrincipal(),
+    })
+
+    // Create the EKS Cluster
+    const cluster = new eks.Cluster(this, "EksCluster", {
+      vpc: vpc,
+      mastersRole: clusterAdmin,
+      defaultCapacity: 2, // Default capacity for worker nodes
+      version: eks.KubernetesVersion.V1_21,
+    })
+
+    // Optionally, enable any EKS add-ons here
+    // e.g., cluster.addHelmChart('my-chart', { ... });
+
+    // Now integrate StackRox security
+    this.deployStackRox(cluster)
+  }
+
+  private deployStackRox(cluster: eks.Cluster) {
+    // StackRox Helm chart installation (replace with your StackRox Helm chart details)
+    const stackRoxNamespace = "stackrox"
+
+    cluster.addHelmChart("StackRoxChart", {
+      repository: "https://charts.stackrox.io",
+      chart: "stackrox-central-services",
+      namespace: stackRoxNamespace,
+      values: {
+        central: {
+          deploymentOverrides: {
+            affinity: {},
+            nodeSelector: {},
+            tolerations: [],
+          },
+          config: {
+            central: {
+              exposure: {
+                type: "NodePort",
+              },
+            },
+          },
+        },
+      },
+    })
+
+    // Additional StackRox components could be installed in a similar way
   }
 }
